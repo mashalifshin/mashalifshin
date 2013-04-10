3169ac865816fcc30a320ef5cb94f88f0d19c0c2
Masha Lifshin <mlifshin@gmail.com>
2013-04-10 16:10:12 -0700

    Initial Rails app, readme, rvmrc commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mashalifshin::Application.config.secret_token = '8a7c2ac7df69ee02963984734a6bcc9b025352dc8924a0b5d75e64c90e6477ee383ca060a6728558e0c97966420b56b14ba86b20fb339d22dfda92eb5f14c205'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Mashalifshin::Application.config.session_store :cookie_store, key: '_mashalifshin_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Mashalifshin::Application.config.session_store :active_record_store
