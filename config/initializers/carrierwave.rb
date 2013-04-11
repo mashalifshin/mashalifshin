0082bb2d11c07b3d8ad1f621615f0e97e0d89e98
Masha Lifshin <mlifshin@gmail.com>
2013-04-10 17:19:33 -0700

    Asset sync, fog, carrierwave configs.

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,15 @@
+CarrierWave.configure do |config|
+  config.root             = Rails.root.join('tmp')
+  config.cache_dir        = 'uploads'
+
+  config.storage          =  :fog
+  config.fog_credentials  = {
+    :provider                   => "AWS",
+    :aws_access_key_id          => ENV['AWS_ACCESS_KEY_ID'],
+    :aws_secret_access_key      => ENV['AWS_SECRET_ACCESS_KEY'],
+  }
+
+  config.fog_directory    = ENV['FOG_DIRECTORY']
+  config.asset_host       = ENV['CLOUDFRONT_DISTRIBUTION_DOMAIN']
+  config.fog_attributes   = {'Cache-Control'=>'max-age=315576000'}
+end
