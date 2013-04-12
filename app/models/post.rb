bd9ee4011db1eff10329592663fbdb2e33a4b431
Masha Lifshin <mlifshin@gmail.com>
2013-04-11 19:58:39 -0700

    Add validations for posts.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,10 @@
 class Post < ActiveRecord::Base
   has_many :comments
 
-  attr_accessible :body, :date, :title
+  attr_accessible :body, :title
+
+  validates :title, :uniqueness => true
+  validates :title, :body, :presence => true
 
   scope :chronological, :order => "created_at DESC"
 end
