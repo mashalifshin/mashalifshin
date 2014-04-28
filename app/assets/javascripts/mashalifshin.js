0ae98f0b8fcc8eda138799db6fe485a06d5d58cd
Masha Lifshin <mlifshin@gmail.com>
2014-04-28 00:07:58 -0700

    Tweak size of symbols in visual header.

--- app/assets/javascripts/mashalifshin.js
+++ app/assets/javascripts/mashalifshin.js
@@ -12,7 +12,7 @@ $(document).ready(function() {
 		var color_g = randomIntInRange(0,256);
 		var color_b = randomIntInRange(0,256);
 		var color_a = parseFloat(randomIntInRange(0,100) / 100);
-		var size = randomIntInRange(50,200);
+		var size = randomIntInRange(1,250);
 		var unicde_character_fun_range = randomIntInRange(8192, 11093);
 		return new_div = $("<span style='color: rgba(" + color_r + "," + color_g + ", " + color_b + ", " + color_a + "); " +
 																"font-size: " + size + "px;'> " +
