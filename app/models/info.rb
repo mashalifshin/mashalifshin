fbf0e451602200718cf0801718d75d6c52618649
Masha Lifshin <mlifshin@gmail.com>
2014-05-05 02:44:54 -0700

    Remove some crest crust.

--- app/models/info.rb
+++ app/models/info.rb
@@ -2,5 +2,4 @@ class Info < ActiveRecord::Base
   attr_accessible :about, :resume, :resume_cache, :remove_resume
 
   mount_uploader :resume, PdfUploader
-  mount_uploader :crest, ImageUploader
 end
