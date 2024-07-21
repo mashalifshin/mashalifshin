e127b04c8ab90bfe3d85db7ed3d541bf89f9090e
masha lifshin <mlifshin@pm.me>
2024-07-21 16:58:44 -0700

    Add more specific cors config

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,11 @@
 # config/initializers/cors.rb
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://mashalifshin.sfo2.digitaloceanspaces.com'
+    origins 'localhost:3000', \
+            '127.0.0.1:3000', \
+            'mashalifshin.onrender.com', \
+            'mashalifshin.com', \
+            'https://mashalifshin.sfo2.digitaloceanspaces.com'
 
     resource '/assets/*',
       headers: :any,
