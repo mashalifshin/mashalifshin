e9ceadd41a7057b1cc33291299105d3ba4b62a83
masha lifshin <mlifshin@pm.me>
2024-07-21 16:19:07 -0700

    Fix font CORS issue, tmp upload directory bug, and tweak 404 style

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,7 @@
 # config/initializers/cors.rb
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'https://mashalifshin.sfo2.digitaloceanspaces.com'
 
     resource '/assets/*',
       headers: :any,
