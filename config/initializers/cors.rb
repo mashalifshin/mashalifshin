3fc15bdb19400a7c52cb07fe918bbaad3c99c886
masha lifshin <mlifshin@pm.me>
2024-07-21 17:06:38 -0700

    Revert "Fix font CORS issue, tmp upload directory bug, and tweak 404 style (#25)"
    This reverts commit d92e4a3e3d43cd8a6f2de374bb9dc586eb6d2885.

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,11 +1,7 @@
 # config/initializers/cors.rb
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:3000', \
-            '127.0.0.1:3000', \
-            'mashalifshin.onrender.com', \
-            'mashalifshin.com', \
-            'https://mashalifshin.sfo2.digitaloceanspaces.com'
+    origins '*'
 
     resource '/assets/*',
       headers: :any,
