bd1e8f6632824bd0d012bedf3113fbcc70fbcd61
masha lifshin <mlifshin@pm.me>
2024-07-21 18:55:08 -0700

    Fix image uploader and CDN url

--- app/uploaders/masha_lifshin_uploader.rb
+++ app/uploaders/masha_lifshin_uploader.rb
@@ -21,7 +21,7 @@ class MashaLifshinUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "/uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
@@ -46,11 +46,10 @@ class MashaLifshinUploader < CarrierWave::Uploader::Base
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
-  def filename
-    if original_filename
-      @hash ||= Digest::MD5.hexdigest(File.dirname(current_path))
-      "#{file.filename.split('_').last}-#{@hash}.#{file.extension}"
-    end
-  end
+  # def filename
+  #   if original_filename
+  #     ...
+  #   end
+  # end
 
 end
